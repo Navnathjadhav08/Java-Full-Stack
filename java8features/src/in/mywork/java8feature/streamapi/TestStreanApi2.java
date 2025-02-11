1963a792ea48930f2b5ae33e482974a0e7bb3c7b
navnathjadhav08 <131193155+Navnathjadhav08@users.noreply.github.com>
2025-02-11 09:48:51 +0530

    StreamApis

---
+++ java8features/src/in/mywork/java8feature/streamapi/TestStreanApi2.java
@@ -0,0 +1,46 @@
+package in.mywork.java8feature.streamapi;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.OptionalInt;
+import java.util.stream.Stream;
+
+public class TestStreanApi2 {
+	public static void main(String[] args) {
+//		String[][] strArray = {{"ABC","BBC"},{"CSC","Dgd"},{"DDD","FSD"}};
+//		
+//		Stream<String[]> strStream = Arrays.stream(strArray);
+//		
+//		long result = strStream.flatMap(strArray1 ->Arrays.stream(strArray1))
+//				.filter(str->str.charAt(0)=='F').count();
+//		
+//		System.out.println(result);
+//		
+   
+//		int[][] intArray = {{1,2,3},{4,5,6},{7,8,9}};
+//
+//		Stream<int[]> intStream = Arrays.stream(intArray);
+//		intStream.flatMapToInt(intArray1->Arrays.stream(intArray1))
+//		.filter(no->no%2==0)
+//		.boxed()
+//		.sorted(Comparator.naturalOrder())
+//		.peek(no->System.out.println("After Filter : "+ no))
+//		.forEach(System.out::println);
+//		
+		
+		
+//		int[][] intArray = {{1,3,7},{4,2,3},{6,2,5}};
+//		Stream<int[]>intStream = Arrays.stream(intArray);
+////		OptionalInt result = intStream.flatMapToInt(arr->Arrays.stream(arr))
+////				.filter(no->no%2==0)
+////				.reduce(0,(a,b)->(a>b)?a:b);
+//		if(result.isPresent())
+//		System.out.println(result.getAsInt());
+//		intStream.close();
+//		
+
+	}
+	
+	
+
+}
